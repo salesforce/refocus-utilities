6e0e72fe5a544bceefd737d595429fb82bd72ff1
Harsh Kothari <harshkothari410@gmail.com>
2018-06-08 15:32:40 -0700

    get latest samples

--- get-latest-samples.js
+++ get-latest-samples.js
@@ -35,6 +35,7 @@ let count = 0;
 
 console.log('Starting to grab data');
 stream.on('data', (found) => {
+  console.log(found.length, count);
   found.forEach((sample) => {
     redis.hgetall(sample)
    .then((s) => {
